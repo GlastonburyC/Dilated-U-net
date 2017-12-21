790010acd70cf6944bdae49fcb1ba928b01a34a5
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-12-21 23:21:58 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,10 +4,12 @@ from keras.models import load_model
 from dilated_unet import bce_dice_loss
 from dilated_unet import dice_coef
 import tifffile as tiff
-from tifffile import imsave
 keras.losses.bce_dice_loss = bce_dice_loss
 keras.metrics.dice_coef = dice_coef
 import numpy as np
+import mahotas as mh
+from matplotlib.image import imsave
+
 model = load_model('weights/normal_unet_noadd.weights')
 
 mean, std_dev = np.mean(test), np.std(test)
@@ -21,5 +23,11 @@ normalize = lambda x: (x - mean) / (std_dev + 1e-10)
 test = normalize(test)
 
 test_preds = model.predict(test)
+
+#  test_preds = np.array(test_preds * 255,dtype='uint8')
+
+# for i in range(0,30):
+#   test_preds[i,] = test_preds[i,] > 160
+
 
 imsave('test/test_predictions/test_preds.tif',test_preds)
