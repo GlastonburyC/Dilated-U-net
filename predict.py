41fd226f48ed42f35e3188c4c43a3fbedcbf4ec5
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-12-22 19:11:46 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,9 +10,8 @@ import numpy as np
 import mahotas as mh
 from tifffile import imsave
 
-model = load_model('weights/normal_unet_noadd.weights')
+model = load_model('weights/normal_unet_20M.weights')
 
-mean, std_dev = np.mean(test), np.std(test)
 normalize = lambda x: (x - mean) / (std_dev + 1e-10)
 
 test = tiff.imread('test/test-volume.tif')
@@ -22,12 +21,7 @@ normalize = lambda x: (x - mean) / (std_dev + 1e-10)
 
 test = normalize(test)
 
-test_preds = model.predict(test)
+#test_preds = model.predict(test)
 
-#  test_preds = np.array(test_preds * 255,dtype='uint8')
-
-# for i in range(0,30):
-#   test_preds[i,] = test_preds[i,] > 160
-
-
-imsave('test/test_predictions/test_preds.tif',test_preds)
+out = model.predict(test,batch_size=10)
+imsave('test/test_predictions/test_preds.tif',out)
